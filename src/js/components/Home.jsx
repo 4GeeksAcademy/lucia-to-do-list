fa2e06480aa51fba0e5cce5b2e9591e2f901ad0c
Lucia <lucia.casasco02@gmail.com>
2025-09-10 13:46:12 +0000

    Lucia to do list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [todos, setTodos] = useState([]);
+	const [inputValue, setInputValue] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value);
+	};
+
+	const handleKeyPress = (e) => {
+		if (e.key === "Enter" && inputValue.trim() !== "") {
+			setTodos([...todos, inputValue.trim()]);
+			setInputValue("");
+		}
+	};
+
+	const handleDeleteTodo = (indexToDelete) => {
+		const newTodos = todos.filter((_, index) => index !== indexToDelete);
+		setTodos(newTodos);
+	};
+
+	return (
+		<div className="container">
+			<div className="todo-container">
+				<h1 className="todo-title">TO DO</h1>
+				
+				<div className="todo-input-container">
+					<input
+						type="text"
+						className="todo-input"
+						placeholder="What needs to be done?"
+						value={inputValue}
+						onChange={handleInputChange}
+						onKeyPress={handleKeyPress}
+					/>
+				</div>
+
+				<div className="todo-list-container">
+					{todos.length === 0 ? (
+						<div className="no-tasks">
+							No hay tareas, añadir tareas
+						</div>
+					) : (
+						<ul className="todo-list">
+							{todos.map((todo, index) => (
+								<li key={index} className="todo-item">
+									<span className="todo-text">{todo}</span>
+									<button
+										className="delete-btn"
+										onClick={() => handleDeleteTodo(index)}
+									>
+										×
+									</button>
+								</li>
+							))}
+						</ul>
+					)}
+					
+					{todos.length > 0 && (
+						<div className="todo-footer">
+							{todos.length} item{todos.length !== 1 ? 's' : ''} left
+						</div>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
